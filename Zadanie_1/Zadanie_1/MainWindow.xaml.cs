849849909d4f6fd86a49710d49f44e23ca430fa4
Sonivais <151126779+Sonivais@users.noreply.github.com>
2023-11-23 21:45:44 +0300

    Add files via upload

---
+++ Zadanie_1/Zadanie_1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Double.TryParse(Texta.Text, out double a))
+            { MessageBox.Show("Поле только для чисел"); }
+            double b;
+            b = Math.Floor(a / 10);
+            double c;
+            c = a % 10;
+            Textc.Text += ("единицы:" + c);
+            Textb.Text += ("десятки:" + b);
+        }
+    }
+}
---
+++ Zadanie_3/Zadanie_3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+         Answer.Clear();
+         if (!Double.TryParse(TextBox.Text, out double K))
+         { MessageBox.Show("Поле только для чисел"); }
+         double D;
+         D = K % 7;
+         //if (D == 7) { D = 0; }
+         Answer.Text += ("День недели " + D);
+        }
+    }
+}
